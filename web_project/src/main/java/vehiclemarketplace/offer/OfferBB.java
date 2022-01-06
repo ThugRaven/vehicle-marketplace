f473031eec74cca591609019a84cf177adeef856
ThugRaven <kamilo1715@wp.pl>
2022-01-06 15:57:34 +0100

    Add offer links
    Link functions need onLoad method because of browser caching when going
    to previous page

--- web_project/src/main/java/vehiclemarketplace/offer/OfferBB.java
+++ web_project/src/main/java/vehiclemarketplace/offer/OfferBB.java
@@ -7,6 +7,7 @@ import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.faces.context.Flash;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -19,6 +20,9 @@ import vehiclemarketplace.dao.ModelDAO;
 import vehiclemarketplace.dao.OfferDAO;
 import vehiclemarketplace.dao.UserDAO;
 import vehiclemarketplace.dao.UserRoleDAO;
+import vehiclemarketplace.entities.BodyStyle;
+import vehiclemarketplace.entities.Generation;
+import vehiclemarketplace.entities.Model;
 import vehiclemarketplace.entities.Offer;
 
 @Named
@@ -65,6 +69,9 @@ public class OfferBB implements Serializable {
 	@Inject
 	FacesContext ctx;
 
+	@Inject
+	Flash flash;
+
 	public String onLoad() throws IOException {
 		Offer loaded = null;
 		if (offer.getIdOffer() != 0) {
@@ -97,6 +104,102 @@ public class OfferBB implements Serializable {
 
 		ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Pomyślnie zakończono ogłoszenie!", null));
 		extcontext.getFlash().setKeepMessages(true);
+
+		return PAGE_OFFERS;
+	}
+
+	public String brandLink() {
+		Offer offerFilter = new Offer();
+		if (offer.getBrand() == null) {
+			try {
+				onLoad();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		offerFilter.setBrand(offer.getBrand());
+		offerFilter.setModel(new Model());
+		offerFilter.setGeneration(new Generation());
+		offerFilter.setBodyStyle(new BodyStyle());
+		flash.put("offerFilter", offerFilter);
+
+		return PAGE_OFFERS;
+	}
+
+	public String modelLink() {
+		Offer offerFilter = new Offer();
+		if (offer.getModel() == null) {
+			try {
+				onLoad();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		offerFilter.setBrand(offer.getBrand());
+		offerFilter.setModel(offer.getModel());
+		offerFilter.setGeneration(new Generation());
+		offerFilter.setBodyStyle(new BodyStyle());
+		flash.put("offerFilter", offerFilter);
+
+		return PAGE_OFFERS;
+	}
+
+	public String generationLink() {
+		Offer offerFilter = new Offer();
+		if (offer.getGeneration() == null) {
+			try {
+				onLoad();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		offerFilter.setBrand(offer.getBrand());
+		offerFilter.setModel(offer.getModel());
+		offerFilter.setGeneration(offer.getGeneration());
+		offerFilter.setBodyStyle(new BodyStyle());
+		flash.put("offerFilter", offerFilter);
+
+		return PAGE_OFFERS;
+	}
+
+	public String fuelLink() {
+		Offer offerFilter = new Offer();
+		if (offer.getFuel() == null) {
+			try {
+				onLoad();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		offerFilter.setBrand(offer.getBrand());
+		offerFilter.setModel(offer.getModel());
+		offerFilter.setGeneration(offer.getGeneration());
+		offerFilter.setBodyStyle(new BodyStyle());
+		offerFilter.setFuel(offer.getFuel());
+		flash.put("offerFilter", offerFilter);
+
+		return PAGE_OFFERS;
+	}
+
+	public String bodyStyleLink() {
+		Offer offerFilter = new Offer();
+		if (offer.getBodyStyle() == null) {
+			try {
+				onLoad();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		offerFilter.setBrand(offer.getBrand());
+		offerFilter.setModel(offer.getModel());
+		offerFilter.setGeneration(offer.getGeneration());
+		offerFilter.setBodyStyle(offer.getBodyStyle());
+		flash.put("offerFilter", offerFilter);
 
 		return PAGE_OFFERS;
 	}
